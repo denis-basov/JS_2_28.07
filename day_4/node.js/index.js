e76ed12ec835c4e7c0b4665110ce575cc237e5d9
Denis <52415012+denis-basov@users.noreply.github.com>
2024-08-25 11:15:40 +0300

    Add files via upload

--- day_4/node.js/index.js
+++ day_4/node.js/index.js
@@ -3,7 +3,24 @@ const http = require('http');
 
 // создаем сервер
 const server = http.createServer(function(request, response){
-    response.end('Server started');
+    const pathName = request.url;// получаю строку запроса URL
+
+    // получаю содержимое html-файлов
+    const tempOverview = fs.readFileSync('./templates/overview.html', 'utf-8');
+    const tempProduct = fs.readFileSync('./templates/product.html', 'utf-8');
+
+    // в зависимости от того, какой url, показываем разные данные
+    if (pathName === '/' || pathName === '/overview') {
+        response.writeHead(200, {'Content-type': 'text/html'});
+        response.end(tempOverview);
+    } else if (pathName === '/product') {
+        response.writeHead(200, {'Content-type': 'text/html'});
+        response.end(tempProduct);
+    } else {
+        response.writeHead(404, {'Content-type': 'text/html'});
+        response.end('<h1>Page not found</h1>');
+    }
+    
 });
 
 // запускаем сервер
